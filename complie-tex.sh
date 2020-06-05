507ed2a18e90a581d97493b6ca49a605ab216da5
wasabilti <2923043785@qq.com>
2020-06-05 11:46:38 +0800

    update comment

--- complie-tex.sh
+++ complie-tex.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Use xelatex to complie .tex file
+# Type :! ./complie.sh under vim can auto complie .tex file use xelatex complier and auto call prewview under macos to view .pdf file
+# Edit FILENAME var before use this script
 
 FILENAME=analyse_report
 
