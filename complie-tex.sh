0a442b77118901cfdfd38476b6a4edb806348da0
wasabilti <2923043785@qq.com>
2020-06-06 16:32:35 +0800

    add cnki2bib.sh

--- complie-tex.sh
+++ complie-tex.sh
@@ -5,9 +5,15 @@
 # Edit FILENAME var before use this script
 
 FILENAME=analyse_report
+set INCLUDE_BIBTEX;
 
 if [ -f $FILENAME.tex ]; then
 	xelatex $FILENAME.tex
+	if [ INCLUDE_BIBTEX ]; then
+		bibtex $FILENAME.aux
+		xelatex $FILENAME.tex
+		xelatex $FILENAME.tex
+	fi
 	open $FILENAME.pdf
 else
 	echo -e 'File '$FILENAME 'does not exist! \nPlease check whether file exist or change FILENAME var to fit your .tex file' >&2
